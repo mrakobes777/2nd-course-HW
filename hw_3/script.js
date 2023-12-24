e3368f5427af81c641a1d01bcbc103cdee164c26
ildar Sadekov <sadekild@gmail.com>
2023-12-24 22:58:23 +0300

    modified:   hw_3/index1.html modified:   hw_3/script.js

---
+++ hw_3/script.js
@@ -0,0 +1,9 @@
+// Задание 1
+let password = Ildar
+let answer = String(prompt('Введите пароль'))
+
+if (password==='Ildar'||'ildar') {console.log('Пароль введен верно')
+    
+} else {console.log('Пароль введен не верно');
+    
+}
