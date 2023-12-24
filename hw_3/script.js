17586ae4c349a34453848f4eb1a8161a027e2fda
ildar Sadekov <sadekild@gmail.com>
2023-12-25 01:41:52 +0300

    modified:   hw_3/script.js

--- hw_3/script.js
+++ hw_3/script.js
@@ -2,8 +2,16 @@
 let password = "Ildar";
 let answer = prompt("Введите пароль");
 
-if (answer === Ildar) {
+if (answer === "Ildar") {
   console.log("Пароль введен верно");
 } else {
   console.log("Пароль введен неверно");
+}
+// Задание 2
+
+let c = prompt("Введите любое число");
+if (c >= 0 && c <= 10) {
+  console.log("Верно");
+} else {
+  console.log("Неверно");
 }
