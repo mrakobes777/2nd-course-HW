5fa802418226ab5c4fbdfed47508a7a34f26554d
ildar Sadekov <sadekild@gmail.com>
2023-12-25 05:53:57 +0300

    modified:   hw_3/script.js

--- hw_3/script.js
+++ hw_3/script.js
@@ -15,3 +15,24 @@ if (c >= 0 && c <= 10) {
 } else {
   console.log("Неверно");
 }
+
+// Задание 3
+
+let d = prompt("Введите число");
+let e = prompt("Введите число");
+if (d > 100 || e > 100) {
+  console.log("Верно");
+} else {
+  console.log("Неверно");
+}
+
+// Задание 4
+
+let a = "2";
+let b = "3";
+a = Number(a);
+b = Number(b);
+alert(a + b);
+
+
+
