3f5741f2250647cec456549c1357c485d001da10
ildar Sadekov <sadekild@gmail.com>
2023-12-25 05:54:22 +0300

    modified:   hw_3/script.js

--- hw_3/script.js
+++ hw_3/script.js
@@ -34,5 +34,58 @@ a = Number(a);
 b = Number(b);
 alert(a + b);
 
+let month = String(prompt("Введите номер месяца"));
+month = Number(month);
 
+switch (month) {
+  case 1:
+    console.log("Зима");
+    break;
 
+  case 2:
+    console.log("Зима");
+    break;
+
+  case 3:
+    console.log("Весна");
+    break;
+
+  case 4:
+    console.log("Весна");
+    break;
+
+  case 5:
+    console.log("Весна");
+    break;
+
+  case 6:
+    console.log("Лето");
+    break;
+
+  case 7:
+    console.log("Лето");
+    break;
+
+  case 8:
+    console.log("Лето");
+    break;
+
+  case 9:
+    console.log("Осень");
+    break;
+
+  case 10:
+    console.log("Осень");
+    break;
+
+  case 11:
+    console.log("Осень");
+    break;
+
+  case 12:
+    console.log("Зима");
+    break;
+}
+if (month > 12) {
+  console.log("Нет такого месяца");
+}
