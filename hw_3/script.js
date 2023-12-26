0bdc157cf8f97c1c6e5eaccda306eafdeee80945
ildar Sadekov <sadekild@gmail.com>
2023-12-26 14:43:13 +0300

    modified:   hw_3/script.js #

--- hw_3/script.js
+++ hw_3/script.js
@@ -1,14 +1,13 @@
 // Задание 1
-let password = "Ildar";
 let answer = prompt("Введите пароль");
-
-if (answer === "Ildar") {
+let password = "Hello";
+if (answer === password) {
   console.log("Пароль введен верно");
 } else {
   console.log("Пароль введен неверно");
 }
-// Задание 2
 
+// Задание 2
 let c = prompt("Введите любое число");
 if (c >= 0 && c <= 10) {
   console.log("Верно");
@@ -34,58 +33,32 @@ a = Number(a);
 b = Number(b);
 alert(a + b);
 
-let month = String(prompt("Введите номер месяца"));
+let month = prompt("Введите номер месяца");
 month = Number(month);
 
 switch (month) {
   case 1:
-    console.log("Зима");
-    break;
-
   case 2:
+  case 12:
     console.log("Зима");
     break;
 
   case 3:
-    console.log("Весна");
-    break;
-
   case 4:
-    console.log("Весна");
-    break;
-
   case 5:
     console.log("Весна");
     break;
-
   case 6:
-    console.log("Лето");
-    break;
-
   case 7:
-    console.log("Лето");
-    break;
-
   case 8:
     console.log("Лето");
     break;
-
   case 9:
-    console.log("Осень");
-    break;
-
   case 10:
-    console.log("Осень");
-    break;
-
   case 11:
     console.log("Осень");
     break;
-
-  case 12:
-    console.log("Зима");
-    break;
-}
-if (month > 12) {
-  console.log("Нет такого месяца");
+  default: 
+    console.log("Неизвестный месяц");
+  
 }
